d2ca97ad48577189d9c56456f6d7449dc403a4c1
Maigné Jean-François <jfmaigne@gmail.com>
2026-02-01 09:46:51 +0100

    Initial Commit

---
+++ AuthTests/AuthTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTests.swift
+//  AuthTests
+//
+//  Created by MAIGNE JEAN-FRANCOIS on 01/02/2026.
+//
+
+import Testing
+@testable import Auth
+
+struct AuthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
